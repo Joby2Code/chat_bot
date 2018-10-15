bf446160aed6e057c6a9f342bdc6b24d0c83d3b7
Joby Joy <jj2196@nyu.edu>
2018-10-15 19:33:45 -0400

    images for chat bot

--- lambda/api_services/app.py
+++ lambda/api_services/app.py
@@ -4,8 +4,8 @@ wordmap = {};
 
 
 def _createWordMap():
-    wordmap["hello"] = ["Hello,Greetings, Nice to meet you!, How are you?"]
-    wordmap["help"] = ["How may I help you?, What products are you interested in ?"]
+    wordmap["hello"] = ["Hello", "Greetings", "Nice to meet you!", " How are you?"]
+    wordmap["help"] = ["How may I help you?", "What products are you interested in ?"]
     wordmap["thank"] = ["You are welcome"]
 
 
@@ -15,8 +15,16 @@ def lambda_handler(event, context):
     for message in event["messages"]:
         text = message["unstructured"]["text"]
 
-    response = wordmap[text]
+    message = wordmap[text]
+    return response(message, 200)
 
+
+def response(message, status_code):
     return {
-        "response": response
+        'statusCode': str(status_code),
+        'message': message,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
     }
